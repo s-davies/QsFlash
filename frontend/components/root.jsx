46ff28a61643af1ba3accabcac5e60f9ce1b1235
s-davies <swdavies.contact@gmail.com>
2020-03-05 09:43:43 -0800

    added scroll to top

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { HashRouter } from 'react-router-dom';
+import ScrollToTop from './scroll_to_top';
 
 import App from './app';
 
 const Root = ({ store }) => (
   <Provider store={store}>
     <HashRouter>
+      <ScrollToTop>
       <App />
+      </ScrollToTop>
     </HashRouter>
   </Provider>
 );
