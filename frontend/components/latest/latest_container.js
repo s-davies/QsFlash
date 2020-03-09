c3979a4bb49bc84f6ce0a547a5897d186adb0ccb
App Academy Student <student@appacademy.io>
2020-03-09 09:55:59 -0700

    recent and latest sorted correctly recursively

--- frontend/components/latest/latest_container.js
+++ frontend/components/latest/latest_container.js
@@ -1,5 +1,6 @@
 import { connect } from 'react-redux';
 import { fetchDecks } from '../../actions/deck_actions';
+import { fetchDeckStudy } from '../../actions/deck_study_actions'
 import { fetchCards } from '../../actions/card_actions';
 import { fetchUsers } from '../../actions/session_actions';
 import Latest from './latest';
@@ -7,6 +8,7 @@ import Latest from './latest';
 const mapStateToProps = (state) => {
     return {
         decks: Object.keys(state.entities.decks).map(key => state.entities.decks[key]),
+        deckStudy: Object.values(state.entities.deckStudies)[0],
         currentUser: state.entities.users[state.session.id],
         users: state.entities.users
     }
@@ -15,7 +17,8 @@ const mapStateToProps = (state) => {
 const mapDispatchToProps = dispatch => ({
     fetchDecks: () => dispatch(fetchDecks()),
     fetchCards: (deckId) => dispatch(fetchCards(deckId)),
-    fetchUsers: () => dispatch(fetchUsers())
+    fetchUsers: () => dispatch(fetchUsers()),
+    fetchDeckStudy: deckId => dispatch(fetchDeckStudy(deckId))
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(Latest);
--- frontend/components/recent/recent.jsx
+++ frontend/components/recent/recent.jsx
@@ -17,15 +17,15 @@ class Recent extends React.Component {
   }
 
   componentDidMount() {
-    this.props.fetchDecks().then(() => this.sortDecks());
+    this.props.fetchDecks().then(() => this.sortDecks(this.props.decks, 0));
     this.props.fetchUsers();
   }
 
-  sortDecks() {
-    let sortedDecks = Object.assign([], this.props.decks.map(deck => Object.assign({}, deck)));
-    for (let i = 0; i < sortedDecks.length; i++) {
-      const deck = sortedDecks[i];
-      if (i === sortedDecks.length - 1) {
+  sortDecks(decks, currentDeckIdx) {
+    let sortedDecks = Object.assign([], decks.map(deck => Object.assign({}, deck)));
+    // for (let i = 0; i < sortedDecks.length; i++) {
+    const deck = sortedDecks[currentDeckIdx];
+    if (currentDeckIdx === sortedDecks.length - 1) {
         this.props.fetchDeckStudy(deck.id)
         .then(() => {
           deck.deckStudyUpdatedAt = this.props.deckStudy.updatedAt
@@ -79,62 +79,14 @@ class Recent extends React.Component {
           });
 
           this.setState({ decks: sortedDecks, decksSorted: true });
-        })
+        });
       } else {
-        this.props.fetchDeckStudy(deck.id).then(() => deck.deckStudyUpdatedAt = this.props.deckStudy.updatedAt)
+        this.props.fetchDeckStudy(deck.id).then(() => {
+          deck.deckStudyUpdatedAt = this.props.deckStudy.updatedAt;
+          this.sortDecks(sortedDecks, currentDeckIdx + 1);
+        });
       }
-    }
-    // debugger
-    // sortedDecks.sort((deck1, deck2) => {
-    //   const createdYear1 = parseInt(deck1.updatedAt.slice(0, 4));
-    //   let createdMonth1 = deck1.updatedAt.slice(5, 7);
-    //   if (createdMonth1[0] === "0") createdMonth1 = createdMonth1.slice(1);
-    //   createdMonth1 = parseInt(createdMonth1);
-    //   let createdDay1 = deck1.updatedAt.slice(8, 10);
-    //   if (createdDay1[0] === "0") createdDay1 = createdDay1.slice(1);
-    //   createdDay1 = parseInt(createdDay1);
-    //   let createdHour1 = deck1.updatedAt.slice(11, 13);
-    //   if (createdHour1[0] === "0") createdHour1 = createdHour1.slice(1);
-    //   createdHour1 = parseInt(createdHour1);
-    //   let createdMinute1 = deck1.updatedAt.slice(14, 16);
-    //   if (createdMinute1[0] === "0") createdMinute1 = createdMinute1.slice(1);
-    //   createdMinute1 = parseInt(createdMinute1);
-    //   let createdSecond1 = deck1.updatedAt.slice(17, 19);
-    //   if (createdSecond1[0] === "0") createdSecond1 = createdSecond1.slice(1);
-    //   createdSecond1 = parseInt(createdSecond1);
-
-    //   const createdYear2 = parseInt(deck2.updatedAt.slice(0, 4));
-    //   let createdMonth2 = deck2.updatedAt.slice(5, 7);
-    //   if (createdMonth2[0] === "0") createdMonth2 = createdMonth2.slice(1);
-    //   createdMonth2 = parseInt(createdMonth2);
-    //   let createdDay2 = deck2.updatedAt.slice(8, 10);
-    //   if (createdDay2[0] === "0") createdDay2 = createdDay2.slice(1);
-    //   createdDay2 = parseInt(createdDay2);
-    //   let createdHour2 = deck2.updatedAt.slice(11, 13);
-    //   if (createdHour2[0] === "0") createdHour2 = createdHour2.slice(1);
-    //   createdHour2 = parseInt(createdHour2);
-    //   let createdMinute2 = deck2.updatedAt.slice(14, 16);
-    //   if (createdMinute2[0] === "0") createdMinute2 = createdMinute2.slice(1);
-    //   createdMinute2 = parseInt(createdMinute2);
-    //   let createdSecond2 = deck2.updatedAt.slice(17, 19);
-    //   if (createdSecond2[0] === "0") createdSecond2 = createdSecond2.slice(1);
-    //   createdSecond2 = parseInt(createdSecond2);
-
-    //   if (createdYear1 > createdYear2) return -1;
-    //   if (createdYear2 > createdYear1) return 1;
-    //   if (createdMonth1 > createdMonth2) return -1;
-    //   if (createdMonth2 > createdMonth1) return 1;
-    //   if (createdDay1 > createdDay2) return -1;
-    //   if (createdDay2 > createdDay1) return 1;
-    //   if (createdHour1 > createdHour2) return -1;
-    //   if (createdHour2 > createdHour1) return 1;
-    //   if (createdMinute1 > createdMinute2) return -1;
-    //   if (createdMinute2 > createdMinute1) return 1;
-    //   if (createdSecond1 > createdSecond2) return -1;
-    //   if (createdSecond2 > createdSecond1) return 1;
-    // });
-
-    // this.setState({ decks: sortedDecks, decksSorted: true });
+    
   }
 
   handleRedirect(deckId) {
